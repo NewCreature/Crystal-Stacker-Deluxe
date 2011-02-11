fbe35e6dc1c0bc0ce3cb0ad1e9e54704835306cb
Todd Cope <xander4jesus@yahoo.com>
2011-02-11 16:06:27 -0600

    Added source and header for interface.

---
+++ src/title.c
@@ -0,0 +1,10 @@
+#include "t3f/t3f.h"
+#include "t3f/gui.h"
+
+void csd_title_logic(void)
+{
+}
+
+void csd_title_render(void)
+{
+}
---
+++ src/title.h
@@ -0,0 +1,7 @@
+#ifndef CSD_TITLE_H
+#define CSD_TITLE_H
+
+void csd_title_logic(void);
+void csd_title_render(void);
+
+#endif
